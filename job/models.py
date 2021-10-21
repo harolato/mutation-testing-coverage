7be13148dbd087b3793fa408ab188553adef6cf9
Haroldas Latonas <haro.lato@gmail.com>
2021-10-21 21:55:25 +0200

    🌱

---
+++ job/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Job(models.Model):
+    hash = models.CharField(max_length=255)
+    pull_request_id = models.CharField(max_length=255)
+    test_cases = models.JSONField()
+
+    class Meta:
+        db_table = 'job'
+
+
+class File(models.Model):
+    hash = models.CharField(max_length=255)
+    path = models.CharField(max_length=255)
+
+    job = models.ForeignKey('Job', on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        db_table = 'file'
+
+
+class Mutation(models.Model):
+    sequence_number = models.IntegerField()
+    description = models.TextField()
+    start_line = models.IntegerField()
+    end_line = models.IntegerField()
+    mutated_source_code = models.TextField()
+    result = models.CharField(max_length=2, choices=[
+        ('S', 'Survived'),
+        ('K', 'Killed'),
+        ('I', 'Inconsistent'),
+    ], default='I')
+
+    file = models.ForeignKey('File', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'mutation'
